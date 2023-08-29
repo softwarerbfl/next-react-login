6fb6b4eb0798850982d01887a9e58524bb6e6b01
softwarerbfl <32202018@dankook.ac.kr>
2023-08-29 13:38:43 +0900

    프로젝트 초기 설정

---
+++ src/app/pages/page.js
@@ -0,0 +1,11 @@
+import ChildCareIcon from '@mui/icons-material/ChildCare';
+import '../globals.css'
+
+function Home() {
+  return <>
+    <h1 className="text-orange-300">응애</h1>
+    <div> <ChildCareIcon className="child"/></div>
+    
+  </>
+}
+export default Home
